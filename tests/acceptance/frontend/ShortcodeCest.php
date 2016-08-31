3daf5c89436789c66b5dd184799160bae37be157
Luca Tumedei <luca@theaveragedev.com>
2016-08-31 17:31:02 +0200

    added small accceptance test

---
+++ tests/acceptance/frontend/ShortcodeCest.php
@@ -0,0 +1,34 @@
+<?php
+namespace acceptance\frontend;
+
+class ShortcodeCest
+{
+
+    /**
+     * @test
+     * it should render simple shortcode
+     */
+    public function it_should_render_simple_shortcode(\AcceptanceTester $I)
+    {
+        $content = 'Lorem ipsum [idlikethis]';
+        $post_id = $I->havePostInDatabase(['post_content' => $content, 'post_name' => 'foo']);
+
+        $I->amOnPage('/foo');
+        $text = "I'd like this";
+        $I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
+    }
+
+    /**
+     * @test
+     * it should render extended shortcode
+     */
+    public function it_should_render_extended_shortcode(\AcceptanceTester $I)
+    {
+        $content = 'Lorem ipsum [idlikethis]Some idea of mine[/idlikethis]';
+        $post_id = $I->havePostInDatabase(['post_content' => $content, 'post_name' => 'foo']);
+
+        $I->amOnPage('/foo');
+        $text = "Some idea of mine";
+        $I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
+    }
+}
