96dc8aff3e22e213b156e80518dcd4bce247faf3
theAverageDev (Luca Tumedei) <luca.tumedei@gmail.com>
2016-08-31 22:36:29 +0200

    Update ShortcodeCest.php

--- tests/acceptance/frontend/ShortcodeCest.php
+++ tests/acceptance/frontend/ShortcodeCest.php
@@ -13,7 +13,7 @@ class ShortcodeCest
         $content = 'Lorem ipsum [idlikethis]';
         $post_id = $I->havePostInDatabase(['post_name' => 'foo', 'post_content' => $content]);
 
-        $I->amOnPage('/foo');
+        $I->amOnPage('/?p=' . $post_id);
         $text = "I'd like this";
         $I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
     }
@@ -27,7 +27,7 @@ class ShortcodeCest
         $content = 'Lorem ipsum [idlikethis]Some idea of mine[/idlikethis]';
         $post_id = $I->havePostInDatabase(['post_name' => 'foo', 'post_content' => $content]);
 
-        $I->amOnPage('/foo');
+        $I->amOnPage('/?p=' . $post_id);
         $text = "Some idea of mine";
         $I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
     }
