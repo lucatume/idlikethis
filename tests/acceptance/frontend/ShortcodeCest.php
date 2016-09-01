1635352f660a4fe236604be40b90f7b60c42d63f
Luca Tumedei <luca@theaveragedev.com>
2016-09-01 17:31:17 +0200

    refreshed the db dump

--- tests/acceptance/frontend/ShortcodeCest.php
+++ tests/acceptance/frontend/ShortcodeCest.php
@@ -1,34 +1,31 @@
 <?php
 namespace acceptance\frontend;
 
-class ShortcodeCest
-{
+class ShortcodeCest {
 
-    /**
-     * @test
-     * it should render simple shortcode
-     */
-    public function it_should_render_simple_shortcode(\AcceptanceTester $I)
-    {
-        $content = 'Lorem ipsum [idlikethis]';
-        $post_id = $I->havePostInDatabase(['post_name' => 'foo', 'post_content' => $content]);
+	/**
+	 * @test
+	 * it should render simple shortcode
+	 */
+	public function it_should_render_simple_shortcode(\AcceptanceTester $I) {
+		$content = 'Lorem ipsum [idlikethis]';
+		$post_id = $I->havePostInDatabase(['post_name' => 'foo', 'post_content' => $content]);
 
-        $I->amOnPage('/foo');
-        $text = htmlspecialchars("I'd like this");
-        $I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
-    }
+		$I->amOnPage('/foo');
+		$text = "I'd like this";
+		$I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
+	}
 
-    /**
-     * @test
-     * it should render extended shortcode
-     */
-    public function it_should_render_extended_shortcode(\AcceptanceTester $I)
-    {
-        $content = 'Lorem ipsum [idlikethis]Some idea of mine[/idlikethis]';
-        $post_id = $I->havePostInDatabase(['post_name' => 'foo', 'post_content' => $content]);
+	/**
+	 * @test
+	 * it should render extended shortcode
+	 */
+	public function it_should_render_extended_shortcode(\AcceptanceTester $I) {
+		$content = 'Lorem ipsum [idlikethis]Some idea of mine[/idlikethis]';
+		$post_id = $I->havePostInDatabase(['post_name' => 'foo', 'post_content' => $content]);
 
-        $I->amOnPage('/foo');
-        $text = htmlspecialchars("Some idea of mine");
-        $I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
-    }
+		$I->amOnPage('/foo');
+		$text = "Some idea of mine";
+		$I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
+	}
 }
