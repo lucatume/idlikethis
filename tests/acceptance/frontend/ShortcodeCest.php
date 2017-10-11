bd853e2b793e0379aa352b4fd3ba9e3e7abcd89c
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-10-11 17:47:47 +0200

    restore tests and activate plugin "manually"

--- tests/acceptance/frontend/ShortcodeCest.php
+++ tests/acceptance/frontend/ShortcodeCest.php
@@ -4,29 +4,35 @@ namespace acceptance\frontend;
 
 class ShortcodeCest {
 
+	public function _before( \AcceptanceTester $I ) {
+		$I->loginAsAdmin();
+		$I->amOnPluginsPage();
+		$I->activatePlugin( 'idlikethis' );
+	}
+
 	/**
 	 * @test
 	 * it should render simple shortcode
 	 */
-	public function it_should_render_simple_shortcode(\AcceptanceTester $I) {
+	public function it_should_render_simple_shortcode( \AcceptanceTester $I ) {
 		$content = 'Lorem ipsum [idlikethis]';
-		$post_id = $I->havePostInDatabase(['post_name' => 'foo', 'post_content' => $content]);
+		$post_id = $I->havePostInDatabase( [ 'post_name' => 'foo', 'post_content' => $content ] );
 
-		$I->amOnPage('/?p=' . $post_id);
+		$I->amOnPage( '/?p=' . $post_id );
 		$text = "I'd like this";
-		$I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
+		$I->seeElement( '.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button' );
 	}
 
 	/**
 	 * @test
 	 * it should render extended shortcode
 	 */
-	public function it_should_render_extended_shortcode(\AcceptanceTester $I) {
+	public function it_should_render_extended_shortcode( \AcceptanceTester $I ) {
 		$text    = 'Some idea of mine';
 		$content = 'Lorem ipsum [idlikethis]' . $text . '[/idlikethis]';
-		$post_id = $I->havePostInDatabase(['post_name' => 'foo', 'post_content' => $content]);
+		$post_id = $I->havePostInDatabase( [ 'post_name' => 'foo', 'post_content' => $content ] );
 
-		$I->amOnPage('/?p=' . $post_id);
-		$I->seeElement('.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button');
+		$I->amOnPage( '/?p=' . $post_id );
+		$I->seeElement( '.idlikethis-button[data-post-id="' . $post_id . '"][data-text="' . $text . '"] button' );
 	}
 }
