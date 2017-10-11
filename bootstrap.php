5202d6d6fde5ad7ee531f9ca93409d376a24402c
theAverageDev (Luca Tumedei) <luca@theaveragedev.com>
2017-10-11 19:58:46 +0200

    add plugin body class

--- bootstrap.php
+++ bootstrap.php
@@ -1,6 +1,12 @@
 <?php
 $container = new tad_DI52_Container();
 
+function idlikethis_body_class(array $classes){
+	$classes['idlikethis'];
+	return $classes;
+}
+
+add_filter('body_class','idlikethis_body_class');
 $container->register('idlikethis_ServiceProviders_Shortcodes');
 $container->register('idlikethis_ServiceProviders_Endpoints');
 $container->register('idlikethis_ServiceProviders_Scripts');
